a7ba7ca5510f3545288beae6f9cd61300f87fd28
Eddy <edward.gonzalez9708@gmail.com>
2018-11-29 19:54:05 -0500

    Added modal for my image.

---
+++ scripts/modal.js
@@ -0,0 +1,16 @@
+let modal = document.getElementById('my-modal')
+
+let img = document.getElementById('my-image')
+
+let modalImg = document.getElementById('img01')
+
+img.onclick = function() {
+    modal.style.display = 'block'
+    modalImg.src = this.src
+}
+
+let span = document.getElementsByClassName('modal-close')[0];
+
+span.onclick = function() {
+    modal.style.display = 'none';
+}
